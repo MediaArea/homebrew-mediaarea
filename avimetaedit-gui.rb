24d0526995f0f044cd434a8e07d872221028be73
Maxime Gervais <gervais.maxime@gmail.com>
2018-05-09 15:50:59 +0200

    avimetaedit-gui 1.0.2

--- avimetaedit-gui.rb
+++ avimetaedit-gui.rb
@@ -1,9 +1,9 @@
 class AvimetaeditGui < Formula
   desc "A tool that supports embedding, validating, and exporting of metadata in AVI (Standard and OpenDML) files (GUI)"
   homepage "https://mediaarea.net/AVIMetaEdit"
-  version "1.0.1"
+  version "1.0.2"
   url "https://mediaarea.net/download/binary/avimetaedit-gui/#{version}/AVIMetaEdit_GUI_#{version}_GNU_FromSource.tar.bz2"
-  sha256 "9e383f4a4a3246d2539c0ffa58ace5b38f6869ad0ac7ffa96ed96aa8188f6fb4"
+  sha256 "abe8a3e267211c6ce1c87eb3af79afee37b793f4e43057aa1104837b3813e978"
 
   depends_on "qt"
 
