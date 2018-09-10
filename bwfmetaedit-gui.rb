466e5288dbe7842cda1c8038271b1483aae6d40b
MediaArea CI <info@mediaarea.net>
2018-09-10 09:15:52 +0000

    bwfmetaedit-gui 1.3.6

--- bwfmetaedit-gui.rb
+++ bwfmetaedit-gui.rb
@@ -1,9 +1,9 @@
 class BwfmetaeditGui < Formula
   desc "A tool that supports embedding, validating, and exporting of metadata in Broadcast WAVE Format (BWF) files (GUI)"
   homepage "https://mediaarea.net/BWFMetaEdit"
-  version "1.3.5"
+  version "1.3.6"
   url "https://mediaarea.net/download/binary/bwfmetaedit-gui/#{version}/BWFMetaEdit_GUI_#{version}_GNU_FromSource.tar.bz2"
-  sha256 "36c7b5fe4c313a0bd3567afbbbf80825ecf41446d86fba0eab39346283f6f376"
+  sha256 "1685a2a492310f4cd86e040239b266b68b72c94f73d570961994f523ea4312ca"
 
   depends_on "qt"
 
