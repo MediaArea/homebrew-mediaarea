a937dba02466cb3e2a8c1bb2f40eb2e97e32e200
MediaArea CI <info@mediaarea.net>
2018-10-23 20:48:53 +0000

    rawcooked 18.10

--- rawcooked.rb
+++ rawcooked.rb
@@ -1,9 +1,9 @@
 class Rawcooked < Formula
   desc "Losless RAW audio-visual data to MKV encoder"
   homepage "https://mediaarea.net/RAWcooked"
-  url "https://mediaarea.net/download/binary/rawcooked/18.09beta1/RAWcooked_CLI_18.09beta1_GNU_FromSource.tar.xz"
-  version "18.09beta1"
-  sha256 "d5be71a09f4c036de6860d88f32f16d1ff7945f093d0b3939139500a9d15c25b"
+  url "https://mediaarea.net/download/binary/rawcooked/18.10/RAWcooked_CLI_18.10_GNU_FromSource.tar.xz"
+  version "18.10"
+  sha256 "f1a1a2226eedf109703009318cca54c4b47ee6d02b08a9fee4ccc022b9ec9c7e"
 
   def install
     cd "Project/GNU/CLI" do
