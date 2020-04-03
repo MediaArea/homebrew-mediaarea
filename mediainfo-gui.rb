08fa56f8830ca308adceb6009c32c332fade8be2
MediaArea CI <info@mediaarea.net>
2020-04-03 12:48:48 +0000

    mediainfo-gui 20.03

--- mediainfo-gui.rb
+++ mediainfo-gui.rb
@@ -1,9 +1,9 @@
 class MediainfoGui < Formula
   desc "Unified display of technical and tag data for audio/video"
   homepage "https://mediaarea.net/"
-  url "https://mediaarea.net/download/binary/mediainfo-gui/19.09/MediaInfo_GUI_19.09_GNU_FromSource.tar.bz2"
-  version "19.09"
-  sha256 "0f0a56a0e7b20291d4a98c7c731c9eea0c9a55236aade84c926452a51d7b8155"
+  url "https://mediaarea.net/download/binary/mediainfo-gui/20.03/MediaInfo_GUI_20.03_GNU_FromSource.tar.bz2"
+  version "20.03"
+  sha256 "f6599b316287d1d5a43657e747f896c7a019d27578cb0fc3fe49fd6cf8e879ae"
 
   depends_on "pkg-config" => :build
   # fails to build against Leopard's older libcurl
