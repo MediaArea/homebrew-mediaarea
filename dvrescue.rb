2222a7dcf8fbda79925aa45619c3c74e90e1c1f5
Maxime Gervais <gervais.maxime@gmail.com>
2020-11-20 09:16:13 +0100

    Update dvrescue to version 0.20.11
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

--- dvrescue.rb
+++ dvrescue.rb
@@ -1,8 +1,8 @@
 class Dvrescue < Formula
   desc "Data migration from DV tapes into files for long-term preservation"
   homepage "https://mediaarea.net/dvrescue"
-  url "https://mediaarea.net/download/binary/dvrescue/0.20.06/dvrescue_CLI_0.20.06_GNU_FromSource.tar.xz"
-  sha256 "259fced760add16fc5153819b9e13bcd0374c616af768ef838b9d4bd9df86530"
+  url "https://mediaarea.net/download/binary/dvrescue/0.20.11/dvrescue_CLI_0.20.11_GNU_FromSource.tar.xz"
+  sha256 "758a427fd426807fdcefcf57224977a76ea009f5f99cdb6317fa1c2ac03c2959"
 
   def install
     cd "ZenLib/Project/GNU/Library" do
