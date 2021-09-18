d5f8e5615f449d0b6582d21463b04c85a118343f
Maxime Gervais <gervais.maxime@gmail.com>
2021-09-19 01:57:35 +0200

    mediainfo-gui v21.09
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

--- mediainfo-gui.rb
+++ mediainfo-gui.rb
@@ -1,13 +1,13 @@
 class MediainfoGui < Formula
   desc "Unified display of technical and tag data for audio/video"
   homepage "https://mediaarea.net/"
-  url "https://mediaarea.net/download/binary/mediainfo-gui/21.03/MediaInfo_GUI_21.03_GNU_FromSource.tar.bz2"
-  sha256 "0ec39683e722a7c3720678b240cc5e5cd7e2ed67544f13b74359b207dc1b63bb"
+  url "https://mediaarea.net/download/binary/mediainfo-gui/21.09/MediaInfo_GUI_21.09_GNU_FromSource.tar.bz2"
+  sha256 "14d617bc11dd43a00a83d3fc5ff793f96b10cc8a962f3990ce90c72630eceec8"
 
   depends_on "pkg-config" => :build
   # fails to build against Leopard's older libcurl
   depends_on "curl" if MacOS.version < :snow_leopard
-  depends_on "wxmac"
+  depends_on "wxwidgets"
 
   def install
     cd "ZenLib/Project/GNU/Library" do
