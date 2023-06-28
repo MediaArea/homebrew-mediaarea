36dc9552807d01581b102c5110c56f23c0cc04a6
Maxime Gervais <gervais.maxime@gmail.com>
2023-06-28 10:26:46 +0200

    mediainfo-gui v23.06
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

--- mediainfo-gui.rb
+++ mediainfo-gui.rb
@@ -1,8 +1,8 @@
 class MediainfoGui < Formula
   desc "Unified display of technical and tag data for audio/video"
   homepage "https://mediaarea.net/"
-  url "https://mediaarea.net/download/binary/mediainfo-gui/23.04/MediaInfo_GUI_23.04_GNU_FromSource.tar.bz2"
-  sha256 "36e7ea20cd43b8618ef18b3dfb6b7122b25e1d00ce6c17fe3258384bb3a37d08"
+  url "https://mediaarea.net/download/binary/mediainfo-gui/23.06/MediaInfo_GUI_23.06_GNU_FromSource.tar.xz"
+  sha256 "c87f1067b83a5e5b7ec62ea2d887398c724965b63674a880991bb76d574cbafa"
 
   depends_on "pkg-config" => :build
   # fails to build against Leopard's older libcurl
