f761b3fe323536888aaccf28ffd0d40e5af3276d
Maxime Gervais <gervais.maxime@gmail.com>
2024-02-02 07:56:05 +0100

    mediainfo-gui v24.01
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

--- mediainfo-gui.rb
+++ mediainfo-gui.rb
@@ -1,12 +1,12 @@
 class MediainfoGui < Formula
   desc "Unified display of technical and tag data for audio/video"
   homepage "https://mediaarea.net/"
-  url "https://mediaarea.net/download/binary/mediainfo-gui/23.11/MediaInfo_GUI_23.11_GNU_FromSource.tar.xz"
-  sha256 "00c51a600f8134a9f41726977461d6d8c44dfa99b4fc4ee6a22135a924b71d8c"
+  url "https://mediaarea.net/download/binary/mediainfo-gui/24.01/MediaInfo_GUI_24.01_GNU_FromSource.tar.xz"
+  sha256 "a4e8e66530334f1e8d8a4da160b34931e62c77deacdaf2b988f65ca6b96d84a7"
 
   depends_on "pkg-config" => :build
   # fails to build against Leopard's older libcurl
-  depends_on "curl" if MacOS.version < :snow_leopard
+  depends_on "curl"
   depends_on "wxwidgets"
 
   def install
