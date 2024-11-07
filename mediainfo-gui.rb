f5ab57ac5c4cd6010ef90e67bb0d2e6ecae17749
Maxime Gervais <gervais.maxime@gmail.com>
2024-11-07 19:10:10 +0100

    mediainfo-gui v24.10
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

--- mediainfo-gui.rb
+++ mediainfo-gui.rb
@@ -1,8 +1,8 @@
 class MediainfoGui < Formula
   desc "Unified display of technical and tag data for audio/video"
   homepage "https://mediaarea.net/"
-  url "https://mediaarea.net/download/binary/mediainfo-gui/24.06/MediaInfo_GUI_24.06_GNU_FromSource.tar.xz"
-  sha256 "dff555b56d7d55e3fa5ecedf5e16f3de76418a64e28973cf31d945d1d4cea7f3"
+  url "https://mediaarea.net/download/binary/mediainfo-gui/24.11/MediaInfo_GUI_24.11_GNU_FromSource.tar.xz"
+  sha256 "e48003f8a347e12675ea47fb1a5e1bf4e98c5858afe80d3bda040046ca1fbd57"
 
   depends_on "pkg-config" => :build
   # fails to build against Leopard's older libcurl
@@ -34,6 +34,6 @@ class MediainfoGui < Formula
   end
 
   test do
-    assert_predicate "#{bin}/mediainfo-gui", :exist?
+    assert_predicate `bin/mediainfo-gui`, :exist?
   end
 end
