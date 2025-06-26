33a44f66b2d6734ea453ff9c04f419a136fa2f8c
Maxime Gervais <gervais.maxime@gmail.com>
2025-06-26 15:15:49 +0200

    ffmpeg-ma: update revision number
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

--- ffmpeg-ma.rb
+++ ffmpeg-ma.rb
@@ -3,6 +3,7 @@ class FfmpegMa < Formula
   homepage "https://github.com/MediaArea/ffmpeg-ma-patch"
   url "https://ffmpeg.org/releases/ffmpeg-7.1.tar.xz"
   sha256 "40973d44970dbc83ef302b0609f2e74982be2d85916dd2ee7472d30678a7abe6"
+  revision 1
   head "git://git.ffmpeg.org/ffmpeg.git", branch: "master"
 
   livecheck do
