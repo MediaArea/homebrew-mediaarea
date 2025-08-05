88b4d31797d6f1e0a4f4ad53b478bc5a3253acdc
Maxime Gervais <gervais.maxime@gmail.com>
2025-08-05 22:54:23 +0200

    ffmpeg-ma: Update to version 7.1.1
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

--- ffmpeg-ma.rb
+++ ffmpeg-ma.rb
@@ -1,9 +1,8 @@
 class FfmpegMa < Formula
   desc "FFmpeg binaries with MediaArea's patches"
   homepage "https://github.com/MediaArea/ffmpeg-ma-patch"
-  url "https://ffmpeg.org/releases/ffmpeg-7.1.tar.xz"
-  sha256 "40973d44970dbc83ef302b0609f2e74982be2d85916dd2ee7472d30678a7abe6"
-  revision 1
+  url "https://ffmpeg.org/releases/ffmpeg-7.1.1.tar.xz"
+  sha256 "733984395e0dbbe5c046abda2dc49a5544e7e0e1e2366bba849222ae9e3a03b1"
   head "git://git.ffmpeg.org/ffmpeg.git", branch: "master"
 
   livecheck do
@@ -28,37 +27,37 @@ class FfmpegMa < Formula
   end
 
   patch do
-    url "https://raw.githubusercontent.com/MediaArea/ffmpeg-ma-patch/8eae8c9dcba927d24e91b87d7d0d9f296187dad1/0001-EXR_consider_float16_as_uint16.patch"
-    sha256 "449f90ce6b22470efa06f5a5722c41d5a40ed957f548c1e05840a51bec3f6723"
+    url "https://raw.githubusercontent.com/MediaArea/ffmpeg-ma-patch/eb736d062433c4220b18815a36c108a452274223/0001-EXR_consider_float16_as_uint16.patch"
+    sha256 "22eeefe6fb84326ecc84caf3ac949287592d46c2332bd8891423b2f505b0b104"
   end
 
   patch do
-    url "https://raw.githubusercontent.com/MediaArea/ffmpeg-ma-patch/8eae8c9dcba927d24e91b87d7d0d9f296187dad1/0002-libavcodec-flacenc-force-32-bit-output-for-32-bit-in.patch"
+    url "https://raw.githubusercontent.com/MediaArea/ffmpeg-ma-patch/eb736d062433c4220b18815a36c108a452274223/0002-libavcodec-flacenc-force-32-bit-output-for-32-bit-in.patch"
     sha256 "713c924fe0b8809912887982c76ca4e32c9d928949f728957bf56e862e572d6e"
   end
 
   patch do
-    url "https://raw.githubusercontent.com/MediaArea/ffmpeg-ma-patch/8eae8c9dcba927d24e91b87d7d0d9f296187dad1/0003-matroskaenc-support-of-TimeCodeXml-input-20231219.patch"
-    sha256 "0d33a8be8c0c436f0559fcb8971ac6e209fce8199526210705af3fbc64548a35"
+    url "https://raw.githubusercontent.com/MediaArea/ffmpeg-ma-patch/eb736d062433c4220b18815a36c108a452274223/0003-matroskadec-enc-support-of-TimeCodeXml-input-add-mk-.patch"
+    sha256 "82c56121ace8c35d5e7d0fb0a9b41b49b27d86ef53d995abd046a14f95d8d2e2"
   end
 
   patch do
-    url "https://raw.githubusercontent.com/MediaArea/ffmpeg-ma-patch/8eae8c9dcba927d24e91b87d7d0d9f296187dad1/0004-avcodec-jpeg2000dec-support-of-2-fields-in-1-AVPacke.patch"
+    url "https://raw.githubusercontent.com/MediaArea/ffmpeg-ma-patch/eb736d062433c4220b18815a36c108a452274223/0004-avcodec-jpeg2000dec-support-of-2-fields-in-1-AVPacke.patch"
     sha256 "2901a5367e1882e4f0415b9b81049a06c240dfc642920426e6e691ab0cfd7fc1"
   end
 
   patch do
-    url "https://raw.githubusercontent.com/MediaArea/ffmpeg-ma-patch/8eae8c9dcba927d24e91b87d7d0d9f296187dad1/0006-avformat-add-DAT-demuxer.patch"
+    url "https://raw.githubusercontent.com/MediaArea/ffmpeg-ma-patch/eb736d062433c4220b18815a36c108a452274223/0006-avformat-add-DAT-demuxer.patch"
     sha256 "2f19a29f7fa828d8c73aaf10f99a3daf7bbad0f4a8ec15b30463848d367bb514"
   end
 
   patch do
-    url "https://raw.githubusercontent.com/MediaArea/ffmpeg-ma-patch/8eae8c9dcba927d24e91b87d7d0d9f296187dad1/0007-configure-evaluate-toolchain-to-determine-suffix.patch"
+    url "https://raw.githubusercontent.com/MediaArea/ffmpeg-ma-patch/eb736d062433c4220b18815a36c108a452274223/0007-configure-evaluate-toolchain-to-determine-suffix.patch"
     sha256 "2e6beada9cb915be7d920aae0f9e1277415acdaa3652ec8f17e7df0afe2c36d2"
   end
 
   patch do
-    url "https://raw.githubusercontent.com/MediaArea/ffmpeg-ma-patch/8eae8c9dcba927d24e91b87d7d0d9f296187dad1/0008-DeckLinkSDK-12.4.2-public-headers.patch"
+    url "https://raw.githubusercontent.com/MediaArea/ffmpeg-ma-patch/eb736d062433c4220b18815a36c108a452274223/0008-DeckLinkSDK-12.4.2-public-headers.patch"
     sha256 "4145f0d358660b7ad8427b549a7724a9df69e845e47c8792ee1eda4d5e2f548a"
   end
 
