8e6bbf600929043996e72e1be7082689ce71f0c0
Maxime Gervais <gervais.maxime@gmail.com>
2025-10-01 09:28:50 +0200

    movmetaedit(-gui): update to 25.09
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

--- movmetaedit-gui.rb
+++ movmetaedit-gui.rb
@@ -1,8 +1,8 @@
 class MovmetaeditGui < Formula
   desc "Embed, validate and export MOV (QuickTime) or MP4 files metadata"
   homepage "https://mediaarea.net/MOVMetaEdit"
-  url "https://mediaarea.net/download/binary/movmetaedit-gui/24.06/MOVMetaEdit_GUI_24.06_GNU_FromSource.tar.xz"
-  sha256 "d743baa9963ea67c9ecdf5a2a01368fffabbef989da493ded978a5d12f01244a"
+  url "https://mediaarea.net/download/binary/movmetaedit-gui/25.09/MOVMetaEdit_GUI_25.09_GNU_FromSource.tar.xz"
+  sha256 "18d42a4c043990df40e5398ea189d940381fd5756902b7c8a47e04c1966d2d40"
 
   depends_on "qt@5"
 
@@ -14,11 +14,11 @@ class MovmetaeditGui < Formula
       prefix.install "MOV MetaEdit.app"
 
       bin.mkpath
-      ln "#{prefix}/MOV MetaEdit.app/Contents/MacOS/MOV MetaEdit", "#{bin}/movmetaedit-gui"
+      ln_s prefix / "MOV MetaEdit.app/Contents/MacOS/MOV MetaEdit", bin / "movmetaedit-gui", relative: true
     end
   end
 
   test do
-    assert_predicate "#{bin}/movmetaedit-gui", :exist?
+    assert_path_exists bin / "movmetaedit-gui"
   end
 end
