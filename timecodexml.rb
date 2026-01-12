220f186fdd5895c8eee4c0cf9f503a3793d3d984
Maxime Gervais <gervais.maxime@gmail.com>
2026-01-12 16:44:14 +0100

    timecodexml: Fix formula lint error
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

--- timecodexml.rb
+++ timecodexml.rb
@@ -3,8 +3,8 @@ class Timecodexml < Formula
   homepage "https://github.com/MediaArea/timecodexml"
   url "https://github.com/MediaArea/timecodexml/archive/refs/tags/v0.2.tar.gz"
   sha256 "5041dba96511d033df2069450115a58f9f171fb2590372dd35ed6895bc38977b"
-  head "https://github.com/MediaArea/timecodexml.git", branch: "main"
   revision 1
+  head "https://github.com/MediaArea/timecodexml.git", branch: "main"
 
   def install
     system "make"
